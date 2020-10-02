72c8e8730fe7919ed812acefd512ae7199235bf7
Kha Truong <khatruong2009@gmail.com>
2020-10-02 16:45:41 -0400

    split up the speakers into individual components

--- forums/src/components/Speakers.jsx
+++ forums/src/components/Speakers.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Speaker from "./Speaker"
 
 function Speakers() {
   return (
@@ -9,135 +10,84 @@ function Speakers() {
                 </div>
 
                 <div className="row">
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Khanna_Parag_FutureMap.jpg" alt="Speaker 1" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Parag Khanna</a></h3>
-                                <p>Founder & Managing Partner, FutureMap</p>
-                                <div className="social">
-                                    <a href="https://www.gvip.io/expertise/4360"><strong style={{fontSize: "larger"}}>GVIP</strong></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Petraeus_David_KKR.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">David H. Petraeus</a></h3>
-                                <p>Chairman, KKR Global Institute and Former Director, Central Intelligence</p>
-                                <div className="social">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Levy_Joaquim_Safra-Bank.jpg" alt="Speaker 3" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Joaquim Levy</a></h3>
-                                <p>Chief Economist, Safra bank and former Minister of Finance, Brazil</p>
-                                <div className="social">
+                    <Speaker 
+                        name="Parag Khanna"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Khanna_Parag_FutureMap.jpg"
+                        description="Founder & Managing Partner, FutureMap"
+                        gvip="https://www.gvip.io/expertise/4360"
+                    />
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Fortea_Javier_Globalvia.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Javier Perez Fortea</a></h3>
-                                <p>CEO, Globalvia</p>
-                                <div className="social">
+                    <Speaker 
+                        name="David H. Petraeus"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Petraeus_David_KKR.jpg"
+                        description="Chairman, KKR Global Institute and Former Director, Central Intelligence"
+                        gvip=""
+                    />
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/norm.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Norman F. Anderson</a></h3>
-                                <p>CG/LA Infrastructure</p>
-                                <div className="social">
-                                    <a href="https://www.gvip.io/expertise/28"><strong style={{fontSize: "larger;"}}>GVIP</strong></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Escoto_Roberto_Apex.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Roberto Escoto</a></h3>
-                                <p>Manager, Investment Department, APEX</p>
-                                <div className="social">
+                    <Speaker  
+                        name="Joaquim Levy"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Levy_Joaquim_Safra-Bank.jpg"
+                        description="Chief Economist, Safra bank and former Minister of Finance, Brazil"
+                        gvip=""
+                    />
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Gluski_Andres_AES.png" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Andrés Gluski</a></h3>
-                                <p> CEO, AES Corporation</p>
-                                <div className="social">
+                    <Speaker 
+                        name="Javier Perez Fortea"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Fortea_Javier_Globalvia.jpg"
+                        description="CEO, Globalvia"
+                        gvip=""
+                    />
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Robb_Andrew_Australia.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Andrew Robb</a></h3>
-                                <p>Minister of Trade and Investment, Government of Australia</p>
-                                <div className="social">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Semsar_Joe_US-Department.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Joseph Semsar</a></h3>
-                                <p>Acting Under Secretary for International Trade, US Department of Commerce Agency</p>
-                                <div className="social">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Pass_James_Guggenheim.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Jim Pass</a></h3>
-                                <p>Senior Managing Director, Guggenheim Partners</p>
-                                <div className="social">
+                    <Speaker 
+                        name="Norman F. Anderson"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/norm.jpg"
+                        description="CG/LA Infrastructure"
+                        gvip="https://www.gvip.io/expertise/28"
+                    />
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-sm-6">
-                        <div className="speaker" data-aos="fade-up" data-aos-delay="200">
-                            <img src="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Jones_Eric_US-International-Finance.jpg" alt="Speaker 2" className="img-fluid w-100"/>
-                            <div className="details">
-                                <h3><a href="#schedule">Eric Jones</a></h3>
-                                <p>Managing Director, U.S. International Development Finance Corporation</p>
-                                <div className="social">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Speaker 
+                        name="Roberto Escoto"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Escoto_Roberto_Apex.jpg"
+                        description="Manager, Investment Department, APEX"
+                        gvip=""
+                    />
+
+                    <Speaker 
+                        name="Andrés Gluski"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Gluski_Andres_AES.png"
+                        description="CEO, AES Corporation"
+                        gvip=""
+                    />
+
+                    <Speaker 
+                        name="Andrew Robb"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Robb_Andrew_Australia.jpg"
+                        description="Minister of Trade and Investment, Government of Australia"
+                        gvip=""
+                    />
+
+                    <Speaker 
+                        name="Joseph Semsar"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Semsar_Joe_US-Department.jpg"
+                        description="Acting Under Secretary for International Trade, US Department of Commerce Agency"
+                        gvip=""
+                    />
+
+                    <Speaker 
+                        name ="Jim Pass"
+                        image ="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/speakers/Pass_James_Guggenheim.jpg"
+                        description="Senior Managing Director, Guggenheim Partners"
+                        gvip=""
+                    />
+
+                    <Speaker 
+                        name="Eric Jones"
+                        image="https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/SpeakersOpt/Jones_Eric_US-International-Finance.jpg"
+                        description="Managing Director, U.S. International Development Finance Corporation"
+                        gvip=""
+                    />
+
                 </div>
             </div>
         </section>
